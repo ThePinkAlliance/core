b050428525ab0a8d1ec7407104d3fcd52474b027
Samuel Villegas <52802289+devsamuelv@users.noreply.github.com>
2022-03-20 21:15:39 -0400

    Changed the file name and removed the dashboard utils

--- src/main/java/com/ThePinkAlliance/core/dashboard/DashboardEntry.java
+++
@@ -1,9 +0,0 @@
-package com.ThePinkAlliance.core.dashboard;
-
-import java.util.function.DoubleSupplier;
-import java.util.function.Supplier;
-
-public interface DashboardEntry {
-  Supplier<Object> stream = () -> 0;
-  String name = "unknown";
-}
