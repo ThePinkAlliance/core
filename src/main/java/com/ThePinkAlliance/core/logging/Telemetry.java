6141fd536e52e78fa141a37a06a17c356b72a80e
devsamuelv <realalloutmusic@gmail.com>
2023-08-14 22:45:34 -0400

    Added telemetry util however the methods need refactoring

--- src/main/java/com/ThePinkAlliance/core/logging/Telemetry.java
+++ src/main/java/com/ThePinkAlliance/core/logging/Telemetry.java
@@ -1,22 +1,18 @@
 package com.ThePinkAlliance.core.logging;
 
 import java.util.HashMap;
-import java.util.List;
-import java.util.NoSuchElementException;
 
-import edu.wpi.first.wpilibj.shuffleboard.ComplexWidget;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardContainer;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
 import edu.wpi.first.wpilibj.shuffleboard.SimpleWidget;
 
 public class Telemetry {
-  private static ShuffleboardContainer tab;
-  private static HashMap<Integer, ShuffleboardLayout> layouts;
-  private static HashMap<String, SimpleWidget> widgets;
+  private static ShuffleboardContainer tab = Shuffleboard.getTab("debug");
+  private static HashMap<Integer, ShuffleboardLayout> layouts = new HashMap<>();
+  private static HashMap<String, SimpleWidget> widgets = new HashMap<>();
 
   public Telemetry() {
-    tab = Shuffleboard.getTab("debug");
   }
 
   public static void logData(String title, String data, Class<?> parent) {
@@ -58,6 +54,28 @@ public class Telemetry {
     }
 
     currentWidget.getEntry().setDouble(data);
+
+    layouts.put(id, layout);
+    widgets.put(title, currentWidget);
+  }
+
+  public static void logData(String title, boolean data, Class<?> parent) {
+    Class<?> superClass = parent.getSuperclass();
+    String className = parent.getSimpleName();
+    String superClassName = superClass != null ? superClass.getSimpleName() : "Unknown";
+    int id = (className + superClassName).hashCode();
+    ShuffleboardLayout layout = layouts.get(id);
+    SimpleWidget currentWidget = widgets.get(title);
+
+    if (layout == null) {
+      layout = tab.getLayout(className + " :: " + superClassName, "List");
+    }
+
+    if (currentWidget == null) {
+      currentWidget = layout.add(title, data);
+    }
+
+    currentWidget.getEntry().setBoolean(data);
 
     layouts.put(id, layout);
     widgets.put(title, currentWidget);
--- src/test/java/LinearInterpolationTest.java
+++ src/test/java/LinearInterpolationTest.java
@@ -3,7 +3,6 @@ import static org.junit.Assert.assertEquals;
 import java.util.List;
 
 import com.ThePinkAlliance.core.math.LinearInterpolationTable;
-import com.ThePinkAlliance.core.math.Vector2d;
 import edu.wpi.first.math.Pair;
 import org.junit.Test;
 
